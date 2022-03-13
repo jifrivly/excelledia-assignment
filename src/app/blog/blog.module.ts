355293ba9155a739922630392209871501feee98
SMH JIFRI <jifrivly@gmail.com>
2022-03-13 22:20:47 +0530

    components and services in blog-module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -5,17 +5,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { PostAddEditComponent } from './post-add-edit/post-add-edit.component';
-import { PostService } from './services';
+import { CommentService, PostService } from 'src/app/blog/services';
 import { PostViewComponent } from './post-view/post-view.component';
+import { CommentAddEditComponent } from './comment-add-edit/comment-add-edit.component';
+import { CommentViewComponent } from './comment-view/comment-view.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
 
 @NgModule({
-    declarations: [BlogComponent, PostAddEditComponent, PostViewComponent],
+    declarations: [BlogComponent, PostAddEditComponent, PostViewComponent, CommentAddEditComponent, CommentViewComponent, CommentListComponent],
     imports: [
         CommonModule,
         BlogRoutingModule,
         FormsModule,
         ReactiveFormsModule,
     ],
-    providers: [PostService],
+    providers: [PostService, CommentService],
 })
 export class BlogModule {}
